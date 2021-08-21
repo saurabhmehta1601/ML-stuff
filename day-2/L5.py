55e9c9839dc734ae3b360cc2f482aa8b5800f2df
saurabh <saurabhmehta1601@gmail.com>
2021-08-21 22:06:22 +0530

    chore(day-3): done resizing cropping images

--- day-2/L5.py
+++ day-2/L5.py
@@ -2,13 +2,28 @@ import numpy as np
 from L3 import rescaleFrame
 import cv2 as cv
 
-
 # translation image
 
 def translate(img, x,y) : 
     transMat = np.float32([[1,0,x],[0,1,y]]) 
     dimensions = (img.shape[1], img.shape[0])
     return cv.warpAffine(img,transMat, dimensions)
+
+# rotate image
+
+def rotate(img,angle,rotPoint=None):
+    (height, width) = img.shape[:2]
+
+    if rotPoint == None :
+        rotPoint = (width//2, height//2)
+    
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimensions = (width, height)
+
+    return cv.warpAffine(img, rotMat, dimensions)
+
+
+
 
 if __name__ =="__main__":
     orignal_img = cv.imread("images/img-1.jpg")
@@ -42,10 +57,20 @@ if __name__ =="__main__":
 
     # cropping
     cropped = img[ 50:200, 200:400]
-    cv.imshow("Cropped image",cropped)
+    # cv.imshow("Cropped image",cropped)
     
     # translate image
     translated = translate(img, 100 , 100)
-    cv.imshow("Translated image", translated)
+    # cv.imshow("Translated image", translated)
     
+    # rotate image
+    rotated = rotate(img , 45) # -ve angle will rotate clockwise
+    # cv.imshow("Rotated image", rotated)
+
+    # flip image
+    flip_h= cv.flip(img , 0) 
+    flip_v= cv.flip(img , 1) 
+    flip_vn= cv.flip(img , -1) 
+    # cv.imshow("Resized image", flip_vn)
+
     cv.waitKey(0)
